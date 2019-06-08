9b0601aa65267b6359f7ab9b7e16261b68acc98a
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-06-08 14:30:05 +0300

    Fixed icon creation on windows

--- CreateIcons.rb
+++ CreateIcons.rb
@@ -2,6 +2,7 @@
 # Creates all the required icon files from the source files
 
 require 'fileutils'
+require 'os'
 
 TARGET_PATH = "assets/icons/"
 SOURCE_IMAGE = "assets/launcher-icon.png"
@@ -10,10 +11,20 @@ FileUtils.mkdir_p TARGET_PATH
 
 abort("icon source doesn't exist") if !File.exists?(SOURCE_IMAGE)
 
+# Windows uses a common magick.exe as the tool name instead of separate executables
+# for all the tools like on Linux
+def command
+  if OS.windows?
+    "magick.exe convert"
+  else
+    "convert"
+  end
+end
+
 
 def createSingleImage(size, ext)
 
-  system("convert #{SOURCE_IMAGE} -resize #{size}x#{size} #{File.join(TARGET_PATH,
+  system("#{command} #{SOURCE_IMAGE} -resize #{size}x#{size} #{File.join(TARGET_PATH,
         size.to_s + 'x' + size.to_s + '.' + ext)}")
 
   abort("converting failed") if $?.exitstatus != 0
@@ -24,23 +35,17 @@ end
 # http://stackoverflow.com/questions/11423711/recipe-for-creating-windows-ico-files-with-imagemagick
 def createMultiSize(ext)
 
-  # system("convert #{SOURCE_IMAGE} -bordercolor white -border 0 " + 
-  #        "\\( -clone 0 -resize 16x16 \\) " +
-  #        "\\( -clone 0 -resize 32x32 \\) " +
-  #        "\\( -clone 0 -resize 48x48 \\) " +
-  #        "\\( -clone 0 -resize 64x64 \\) " +
-  #        "-delete 0 -alpha off -colors 256 #{File.join(TARGET_PATH, 'icon.' + ext)}")
-
   # This version keeps transparency
-  system("convert #{SOURCE_IMAGE} " + 
-         "\\( -clone 0 -resize 16x16 \\) " +
-         "\\( -clone 0 -resize 32x32 \\) " +
-         "\\( -clone 0 -resize 48x48 \\) " +
-         "\\( -clone 0 -resize 64x64 \\) " +
-         "\\( -clone 0 -resize 128x128 \\) " +
-         "\\( -clone 0 -resize 256x256 \\) " +
-         "-delete 0 #{File.join(TARGET_PATH, 'icon.' + ext)}")
-  
+  system("#{command} #{SOURCE_IMAGE} " +
+         [16, 32, 48, 64, 128, 256].map{|res|
+           # Windows doesn't need escapes and doesn't support them
+           if OS.windows?
+             "( -clone 0 -resize #{res}x#{res} )"
+           else
+             "\\( -clone 0 -resize #{res}x#{res} \\)"
+           end
+         }.join(" ") +
+         " -delete 0 -colors 256 #{File.join(TARGET_PATH, 'icon.' + ext)}")
 end
 
 # Wanted png sizes
@@ -55,7 +60,7 @@ createSingleImage(16, "png")
 createSingleImage(32, "png")
 
 # Apparently this doesn't work for some reason
-system("convert " + [16, 32, 64, 128, 256].reverse().map{|a| File.join(TARGET_PATH,
+system("#{command} " + [16, 32, 64, 128, 256].reverse().map{|a| File.join(TARGET_PATH,
                 a.to_s + 'x' + a.to_s + '.png')}.join(' ') + " " +
        File.join(TARGET_PATH, 'icon.icns'))
 
