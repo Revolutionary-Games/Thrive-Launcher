79d0da837e9227cd45f0601a2fe82b33428842bb
Jamie Ridding <jamie.ridding@hotmail.co.uk>
2019-11-28 02:19:29 +0000

    Fix linter errors.

--- utils.js
+++ utils.js
@@ -1,19 +1,21 @@
 // Implementation taken from https://stackoverflow.com/a/18650828/9277476.
 /**
  * Convert a raw number of bytes into a human-readable notation.
- * 
+ *
  * @param {string | number} bytes 
  */
 function formatBytes(bytes, precision = 2) {
-  const units = ["B", "KiB", "MiB", "GiB", "TiB", "PiB"];
-  const kb = 1024;
+	const units = ["B", "KiB", "MiB", "GiB", "TiB", "PiB"];
+	const kb = 1024;
 
-  if (bytes == 0) {
-    return "0 B";
-  }
+	if (bytes == 0) {
+		return "0 B";
+	}
 
-  const unit = Math.floor(Math.log(Number(bytes)) / Math.log(kb));
-  return (Number(bytes) / Math.pow(kb, unit)).toFixed(Math.max(precision, 0)) + " " + units[unit];
+    const unit = Math.floor(Math.log(Number(bytes)) / Math.log(kb));
+    
+    return (Number(bytes) / Math.pow(kb, unit))
+        .toFixed(Math.max(precision, 0)) + " " + units[unit];
 }
 
 exports.formatBytes = formatBytes;
