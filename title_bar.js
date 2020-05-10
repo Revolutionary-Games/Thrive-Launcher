26c492a68028c6bdc550cb4090df91e5c473bf5b
icedjuro <59157446+icedjuro@users.noreply.github.com>
2020-05-10 00:45:42 -0700

    Added custom titlebar (#54)
    * Added custom titlebar
    
    * Fixed styling
    
    * Fixed styling again
    
    * Reformatted CSS
    
    * Refactored code
    
    * Fixed styling
    
    * Fix line endings 1/2
    
    * Fix line endings 2/2
    
    * Another attempt to fix line endings
    
    * Added custom titlebar
    
    Fixed styling
    
    Fixed styling again
    
    Reformatted CSS
    
    Refactored code
    
    Fixed styling
    
    Fix line endings 1/2
    
    Fix line endings 2/2
    
    Another attempt to fix line endings
    
    * Fixed code styling
    
    * Custom made icons and make titlebar always on top
    
    * Fixed open string bug
    
    * Reverted to linux linebreaks
    
    * Whitespace fix
    
    * Whitespace should be fixed this time
    
    Co-authored-by: Henri Hyyryläinen <henri.hyyrylainen@gmail.com>

---
+++ title_bar.js
@@ -0,0 +1,19 @@
+// Adds functionality to custom title bar
+"use strict";
+
+const remote = require("electron").remote;
+const win = remote.getCurrentWindow();
+
+const loadTitleBar = () => {
+    const windowCloseButton = document.getElementById("windowClose");
+    const windowMaximizeButton = document.getElementById("windowMaximize");
+    const windowMinimizeButton = document.getElementById("windowMinimize");
+
+    windowCloseButton.addEventListener("click", () => win.close());
+    windowMaximizeButton.addEventListener("click", () => {
+        win.isMaximized() ? win.unmaximize() : win.maximize();
+    });
+    windowMinimizeButton.addEventListener("click", () => win.minimize());
+};
+
+exports.loadTitleBar = loadTitleBar;
