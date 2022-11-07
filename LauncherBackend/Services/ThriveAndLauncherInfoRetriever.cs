ff98fc7fe82b01b471724d332e876cc9a7b86da8
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-11-07 16:34:53 +0200

    Fixed missing nullability on one string variable

--- LauncherBackend/Services/ThriveAndLauncherInfoRetriever.cs
+++ LauncherBackend/Services/ThriveAndLauncherInfoRetriever.cs
@@ -143,7 +143,7 @@ public class ThriveAndLauncherInfoRetriever : IThriveAndLauncherInfoRetriever
                 () => ResourceUtilities.ReadManifestResourceRawAsync(resourceName), resourceName));
         }
 
-        string signedWith;
+        string? signedWith;
         try
         {
             signedWith = await dataHandler.VerifySignature(data, signature, keys);
